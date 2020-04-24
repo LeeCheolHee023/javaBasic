6d80e6d12079bd9e435338ce5b8df4fd607814a9
cheolhee <lch9394@naver.com>
2020-04-24 12:45:54 +0900

    second

---
+++ BooleanExample.java
@@ -0,0 +1,19 @@
+
+public class BooleanExample{
+
+	public static void main(String[] args){
+		boolean isTure = false;
+		isTure = true;
+		
+		int num1, num2;
+		num1 = 19;
+		num2 = 29;
+		
+		if(num1 > num2){
+		System.out.println("num1 is larger than num2");
+		}
+		else {
+		System.out.println("num2 is larger than num1");
+		}
+	}
+}
