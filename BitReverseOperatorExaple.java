6e93b4c463ab3700a4900fdf8413bf78a477c636
cheolhee <lch9394@naver.com>
2020-04-27 09:41:18 +0900

    add

---
+++ BitReverseOperatorExaple.java
@@ -0,0 +1,14 @@
+
+public class BitReverseOperatorExample {
+  public static void main(String[] args {
+	  int num = 10;
+	  String returnValue = toBinaryString(num1);
+	  System.out.println(returnValue);
+    }
+	public static String toBinaryString(int val){
+		String str = Integer.toBinaryString(val);
+		return str;
+	
+	}
+
+}
